d3513c1ba9b4c398d5d1d2db5d4a7f5dfd8ed5d6
Cobitcoin <cobit@cobitcoin.io>
2018-06-25 17:57:48 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var api = 'https://blockchain.cobitcoin.io/blockapi';
+var blockTargetInterval = 120;
+var coinUnits = 100000;
+var symbol = 'CBC';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "cbc": [
+		["https://api.cobitcoin.io","pool.cobitcoin.io", "cbc"]
+		
+    ]
+};
